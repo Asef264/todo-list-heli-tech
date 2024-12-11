592406b5f5eee6f32eaacb6a17efeaa4ad577370
asef264 <xhmoazedi@gmail.com>
2024-12-11 16:26:08 +0330

    add returning values to query

--- internal/ports/queries.go
+++ internal/ports/queries.go
@@ -1,6 +1,8 @@
 package ports
 
 const (
-	CreateTodoItemQuery = `INSERT INTO todo_items (id, description, due_date, file_id) 
-			  VALUES ($1, $2, $3, $4) RETURNING id, description, due_date, file_id`
+	CreateTodoItemQuery = `
+		INSERT INTO todo_items (id, description, due_date, file_id)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, description, due_date, file_id, created_at`
 )
