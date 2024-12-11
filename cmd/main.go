fead66456298897c43a6b76a5f3f0774bdb96ec2
asef264 <xhmoazedi@gmail.com>
2024-12-12 01:57:32 +0330

    main

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 	"todo-list/api/router"
 	"todo-list/config"
 	"todo-list/internal/adapters/persistence"
@@ -12,10 +13,7 @@ import (
 
 func main() {
 	app, cfg, db := Init()
-
-	// Ensure the database connection is closed when the server stops
 	defer db.Close()
-	// Start the server
 	log.Printf("Server is running on port %d\n", cfg.Server.Port)
 	if err := app.Run(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
@@ -23,26 +21,24 @@ func main() {
 }
 
 func Init() (*gin.Engine, *config.Config, *sql.DB) {
-	// Load configuration
+	if err := os.Setenv("STORAGE_TYPE", "minio"); err != nil {
+		log.Fatalf("error on set storage type,%e", err)
+	}
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
 
-	// Set up database connection
 	db, err := persistence.NewDB(cfg)
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
 	log.Println("Database connection established successfully!")
 
-	// Run database migrations
 	persistence.MigrateUp(db, "migrations", cfg.DB.DBName)
 
-	// Initialize Gin router
 	app := gin.Default()
 
-	// Register routes
 	router.RegisterRoutes(app, db, cfg)
 
 	return app, cfg, db
