df1d4716b2de17907093e5c0d1d5f67de9594d44
asef264 <xhmoazedi@gmail.com>
2024-12-12 01:59:46 +0330

    init postgres and migrations

--- internal/adapters/persistence/postgres.go
+++ internal/adapters/persistence/postgres.go
@@ -28,7 +28,6 @@ func NewDB(cfg *config.Config) (*sql.DB, error) {
 		return nil, err
 	}
 
-	// Test the connection
 	if err := db.Ping(); err != nil {
 		return nil, err
 	}
@@ -36,15 +35,12 @@ func NewDB(cfg *config.Config) (*sql.DB, error) {
 	return db, nil
 }
 
-// MigrateUp is a function to migrate the database
 func MigrateUp(db *sql.DB, filesPath, dbName string) {
-	// Read all migration files from the /migrations directory
-	migrationFiles, err := getMigrationFiles("./migrations")
+	migrationFiles, err := getMigrationFiles("../migrations")
 	if err != nil {
 		log.Fatalf("error reading migration files: %v", err)
 	}
 
-	// Apply each migration file
 	for _, file := range migrationFiles {
 		err := applyMigration(db, file)
 		if err != nil {
@@ -56,14 +52,12 @@ func MigrateUp(db *sql.DB, filesPath, dbName string) {
 	log.Println("Migration applied successfully.")
 }
 
-// getMigrationFiles returns a sorted list of migration file paths from the migrations directory.
 func getMigrationFiles(dir string) ([]string, error) {
 	var files []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		// Check if it's a .sql file
 		if filepath.Ext(path) == ".sql" {
 			files = append(files, path)
 		}
@@ -73,21 +67,15 @@ func getMigrationFiles(dir string) ([]string, error) {
 		return nil, err
 	}
 
-	// Optionally, sort files in a way you want (e.g., lexicographically)
-	// sort.Strings(files)
-
 	return files, nil
 }
 
-// applyMigration reads a SQL migration file and executes its contents.
 func applyMigration(db *sql.DB, file string) error {
-	// Read the migration file content
 	migrationContent, err := ioutil.ReadFile(file)
 	if err != nil {
 		return fmt.Errorf("error reading migration file %s: %v", file, err)
 	}
 
-	// Execute the migration SQL query
 	_, err = db.Exec(string(migrationContent))
 	if err != nil {
 		return fmt.Errorf("error executing migration %s: %v", file, err)
